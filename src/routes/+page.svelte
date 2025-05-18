0006fbb8704fe9685c4a12d9f2a164de5ceaca0f
Alexis Raya <aar335@drexel.edu>
2025-05-17 23:27:08 -0400

    fix font-weight issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to Shakespeare Unscripted</h1>
+<h1>Welcome to Shakespeare</h1>
+<h1 class="emphasis">Unscripted</h1>
 <a href='/play'>Write your story</a>
+
+<style>
+    h1 {
+        font-style: normal;
+        font-size: 3rem;
+        text-align: center;
+        margin: 0;
+    }
+    .emphasis {
+        color: #DE0000;
+        font-style: italic;
+    }
+</style>
