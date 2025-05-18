e380490313f550428956e4e66bba3d6ead86d3d6
Alexis Raya <aar335@drexel.edu>
2025-05-17 23:37:54 -0400

    add home and character choice styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
-<h1>Welcome to Shakespeare</h1>
-<h1 class="emphasis">Unscripted</h1>
-<a href='/play'>Write your story</a>
+<div class="home_container">
+    <div class="title">
+        <h1>Welcome to Shakespeare</h1>
+        <h1 class="emphasis">Unscripted</h1>
+    </div>
+    
+    <a href='/play'>Write your story</a>    
+</div>
+
 
 <style>
     h1 {
@@ -8,6 +14,18 @@
         font-size: 3rem;
         text-align: center;
         margin: 0;
+    }
+
+    a {
+        color: #000;
+        font-size: 1.5rem;
+    }
+    .home_container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        row-gap: 64px;
     }
     .emphasis {
         color: #DE0000;
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,35 @@
+<script>
+    import Proteus from '$lib/assets/Proteus.png'
+</script>
+
 <h1>Choose your character</h1>
 <div class="characters-container">
-    <a href="/play/two-gentlemen">Proteus</a>
+    <a href="/play/two-gentlemen" class="character-choice">
+        <img src={Proteus} alt="Proteus" />
+        <h3>Proteus</h3>
+    </a>
+   
     <!-- <a href="/">Bassanio</a>
     <a href="/">Petruchio</a> -->
 </div>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    a {
+        text-decoration: none;
+        color: #000;
+    }
+    .character-choice {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .character-choice img{
+        width: 300px;
+        height: 400px;
+    }
+</style>
