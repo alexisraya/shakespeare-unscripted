21bdb7f4ae8d3ff5f462f513e64965d2470291b1
Alexis Raya <aar335@drexel.edu>
2025-05-17 23:27:19 -0400

    hide unavailable stories

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Choose your character</h1>
 <div class="characters-container">
     <a href="/play/two-gentlemen">Proteus</a>
-    <a href="/">Bassanio</a>
-    <a href="/">Petruchio</a>
+    <!-- <a href="/">Bassanio</a>
+    <a href="/">Petruchio</a> -->
 </div>
